c3b6504c86da1430a593b4a1461e596773f207fc
rnico2293 <rnico2293@gmail.com>
2024-03-07 12:11:37 -0300

    Creation of authentification routes and internal routes.

--- src/JorunalApp.jsx
+++ src/JorunalApp.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { AppRouter } from './router/AppRouter';
 
 export const JorunalApp = () => {
     return (
         <>
-            <h1>Jorunal App</h1>
+            <AppRouter />
         </>
     )
 }
---
+++ src/auth/pages/LoginPage.jsx
@@ -0,0 +1,8 @@
+
+export const LoginPage = () => {
+    return (
+        <>
+            <h1>COSO COLOR AL CUADRADO</h1>
+        </>
+    )
+}
